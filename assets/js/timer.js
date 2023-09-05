bae54d66e7d5613181c12f421cb40d0c927437f0
burakmrc <burakmeric210@gmail.com>
2023-09-06 01:33:42 +0300

    devfest responsive eklendi

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,6 +1,6 @@
 const year = new Date().getFullYear();
 const fourthOfJuly = new Date(year, 6, 4).getTime();
-const fourthOfJulyNextYear = new Date(year + 1, 6, 4).getTime();
+const fourthOfJulyNextYear = new Date(year + 1, 2, 2).getTime();
 const month = new Date().getMonth();
 
 // countdown
